101b7b70b986a5170374f83d08075fa0b006b864
Jaclav <kancelariajkw@gmail.com>
2022-03-24 21:55:40 +0100

    Adding grid

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 run:
-	g++ main.cpp Cube.cpp -Wall -Wextra -pedantic -Wfatal-errors -lsfml-graphics -lsfml-system -lsfml-window -lGL -lGLEW -o main
-	./main
+	g++ main.cpp Cube.cpp -Wall -Wextra -pedantic -Wfatal-errors -lsfml-graphics -lsfml-system -lsfml-window -lGL -lGLEW
+	./a.out
--- main.cpp
+++ main.cpp
@@ -38,32 +38,42 @@ int main() {
     glCullFace(GL_BACK);
     glPointSize(20.0);
 
+    std::string map[] = {"########",
+                         "#   #  #",
+                         "@   !  #",
+                         "@  !!  #",
+                         "@      #",
+                         "########"
+                        };//! (x,y) element of map is map[z][x]
+
     //Cube
-    Cube cube;
-    cube.setTexture("rsc/Brick.jpg");
+    Cube brick;
+    brick.setTexture("rsc/Brick.jpg");
+
+    Cube wood;
+    wood.setTexture("pics/wood.png");
+
+    Cube redBrick;
+    redBrick.setTexture("pics/redbrick.png");
+
+    Cube notFound;
+    notFound.setTexture("rsc/notFound.png");
+
     std::vector<glm::vec3>positions;
-    positions.push_back(glm::vec3(1, 0, 3));
-    positions.push_back(glm::vec3(0, 0, 0));
-    positions.push_back(glm::vec3(1, 0, 0));
-    positions.push_back(glm::vec3(2, 0, 0));
-    positions.push_back(glm::vec3(0, 0, 2));
-    positions.push_back(glm::vec3(2, 0, 2));
-
-    positions.push_back(glm::vec3(0, 1, 0));
-
-    /*for(int x = 0; x < 100; x++) {
-        for(int y = 0; y < 100; y++) {
-            positions.push_back(glm::vec3(x, y, 8));
+    for(unsigned int z = 0; z < 6; z++) {
+        for(unsigned int x = 0; x < 8; x++) {
+            if(map[z][x] != ' ')
+                positions.push_back(glm::vec3(x, 0, z));
         }
-    }*/
+    }
 
     //camera
     sf::Clock gravityClock;
+    glm::vec3 velocity = glm::vec3(0.1); //! x is velocity for x and y
     bool gravity = true;
-    float velocity = 0.1;
     glm::vec3 direction(0.0, 0, 0);
     glm::vec3 right(0, 0, 0);
-    glm::vec3 cameraPos(0, 0, 1);
+    glm::vec3 cameraPos(1, 0, 1);
 
     glm::mat4 viewMatrix = glm::lookAt(cameraPos, cameraPos + direction, glm::cross(right, direction));
 
@@ -88,36 +98,9 @@ int main() {
             if(event.type == sf::Event::Closed)
                 window.close();
             if(event.type == sf::Event::KeyPressed) {
-                if(sf::Keyboard::isKeyPressed(sf::Keyboard::LControl)) {
-                    velocity = 0.5;
-                }
-                else {
-                    velocity = 0.1;
-                }
-                if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-                    if(gravity)
-                        cameraPos += glm::vec3(direction.x, 0, direction.z) * velocity;//going forward shouldn't case jumping
-                    else
-                        cameraPos += direction * velocity;
-                }
-                if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-                    if(gravity)
-                        cameraPos -= glm::vec3(direction.x, 0, direction.z) * velocity;
-                    else
-                        cameraPos -= direction * velocity;
-                }
-                if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-                    cameraPos -= right * velocity;
-                if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-                    cameraPos += right * velocity;
-                if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
-                    cameraPos += glm::vec3(0, 0.4, 0);
-                if(sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
-                    cameraPos += glm::vec3(0, -0.1, 0);
                 if(sf::Keyboard::isKeyPressed(sf::Keyboard::G))
                     gravity = !gravity;
-            }//end KeyPressed
-
+            }
             if(event.type == event.MouseMoved) {
                 horizontal_angle += float((int)window.getSize().x / 2 -
                                           sf::Mouse::getPosition().x) / (float)window.getSize().x;
@@ -148,20 +131,74 @@ int main() {
                     FOV += 0.1f;
                 perspectiveMatrix = glm::perspective(FOV, aspect, P1, P2);
             }
-
-            //cannot go under ground
-            if(cameraPos.y < 0.5 && gravity) {
-                cameraPos.y = 0.5;
-            }
-            viewMatrix = glm::lookAt(cameraPos, cameraPos + direction, glm::cross(right, direction));
+        }//end event
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::LControl)) {
+            velocity.x = 0.3;
+            velocity.z = 0.3;
         }
+        else {
+            velocity.x = 0.1;
+            velocity.z = 0.1;
+        }
+        //mowing
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+            if(gravity) {
+                cameraPos += glm::vec3(direction.x, 0, direction.z) * velocity.x; //?going forward shouldn't case jumping
+                if(map[(int)std::round(cameraPos.z)][(int)std::round(cameraPos.x)] != ' ')
+                    cameraPos -= glm::vec3(direction.x, 0, direction.z) * velocity.x * 1.6f;
+            }
+            else
+                cameraPos += direction * velocity.x;
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+            if(gravity)
+                cameraPos -= glm::vec3(direction.x, 0, direction.z) * velocity.x;
+            else
+                cameraPos -= direction * velocity.x;
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+            cameraPos -= right * velocity;
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+            cameraPos += right * velocity;
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+            if(velocity.y == 0)
+                velocity.y = 3;
+            if(!gravity)
+                cameraPos += glm::vec3(0, 0.3, 0);
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
+            cameraPos += glm::vec3(0, -0.1, 0);
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            horizontal_angle -= 0.3;
+            direction = glm::vec3(cos(vertical_angle) * sin(horizontal_angle),
+                                  sin(vertical_angle),
+                                  cos(vertical_angle) * cos(horizontal_angle));
+            right = glm::vec3(-cos(horizontal_angle), 0,
+                              sin(horizontal_angle));
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+            horizontal_angle += 0.3;
+            direction = glm::vec3(cos(vertical_angle) * sin(horizontal_angle),
+                                  sin(vertical_angle),
+                                  cos(vertical_angle) * cos(horizontal_angle));
+            right = glm::vec3(-cos(horizontal_angle), 0,
+                              sin(horizontal_angle));
+        }
+
         //gravity
-        if(gravity && cameraPos.y > 0.5 && gravityClock.getElapsedTime().asMilliseconds() > 50) {
-            cameraPos.y -= 0.05;
+        if(gravity) {
+            if(cameraPos.y > 0.5)
+                velocity.y -= gravityClock.getElapsedTime().asMilliseconds() / 1000.0f * 10;
+            cameraPos.y += velocity.y * gravityClock.getElapsedTime().asMilliseconds() / 1000.0f;
             gravityClock.restart();
-            viewMatrix = glm::lookAt(cameraPos, cameraPos + direction, glm::cross(right, direction));
+        }
+        //cannot go under ground
+        if(cameraPos.y < 0.5 && gravity) {
+            cameraPos.y = 0.5;
+            velocity.y = 0;
         }
         //drawing
+        viewMatrix = glm::lookAt(cameraPos, cameraPos + direction, glm::cross(right, direction));
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         std::sort(positions.begin(), positions.end(), [&](glm::vec3 a, glm::vec3 b) {
@@ -175,9 +212,28 @@ int main() {
             return false;
         });
         for(auto i : positions) {
-            cube.setPosition(i);
             //TODO: don't draw not displayed cubes
-            cube.draw(perspectiveMatrix * viewMatrix);
+            switch(map[(int)i.z][(int)i.x]) {
+            case '#': {
+                brick.setPosition(i);
+                brick.draw(perspectiveMatrix * viewMatrix);
+            }
+            break;
+            case '!': {
+                wood.setPosition(i);
+                wood.draw(perspectiveMatrix * viewMatrix);
+            }
+            break;
+            case '@': {
+                redBrick.setPosition(i);
+                redBrick.draw(perspectiveMatrix * viewMatrix);
+            }
+            break;
+            default: {
+                notFound.setPosition(i);
+                notFound.draw(perspectiveMatrix * viewMatrix);
+            }
+            }
         }
 
         //dimension lines
@@ -188,14 +244,6 @@ int main() {
         glBegin(GL_POINTS);
         glVertex3f(0.0, 0, 0);
         glEnd();
-
-       /* glBegin(GL_POINTS);
-        for(float x = -5; x < 5; x += 0.03) {
-            for(float y = -5; y < 5; y += 0.03) {
-                glVertex3f(x, y, x * x + y * y);
-            }
-        }
-        glEnd();*/
 
         glBegin(GL_LINES);
         glVertex3f(0, 0, 0);
@@ -209,6 +257,15 @@ int main() {
         glEnd();
 
         window.display();
-        window.setTitle(std::to_string(fpsCounter()) + "@Wolf3D" + std::to_string(FOV));
+        for(int z = 0; z < 6; z++) {
+            for(int x = 0; x < 8; x++) {
+                if(x == (int)cameraPos.x && z == (int)cameraPos.z)
+                    std::cout << '|';
+                else
+                    std::cout << map[z][x];
+            }
+            std::cout << '\n';
+        }
+        window.setTitle(std::to_string(fpsCounter()) + "@Wolf3D:" + std::to_string(FOV) + std::to_string(cameraPos.x) + ":" + std::to_string(cameraPos.y) + ":" + std::to_string(cameraPos.z));
     }
 }
